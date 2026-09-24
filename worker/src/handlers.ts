a4236e9abe092388573f47dbb676e34dd47642ae
Vinicius <vinisaraiva@gmail.com>
2026-09-24 11:41:18 -0300

    worker: route media analysis jobs

--- worker/src/handlers.ts
+++ worker/src/handlers.ts
@@ -1,7 +1,10 @@
 import type { WorkerJob } from "./queue.js";
+import { handleMediaAnalysis } from "./media-analysis.js";
 
 export async function handleJob(job: WorkerJob) {
   switch (job.type) {
+    case "media_analysis":
+      return handleMediaAnalysis(job);
     default:
       throw new Error(`Unsupported job type: ${job.type}`);
   }
