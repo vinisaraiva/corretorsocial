3d35e9b5c725ca1a8d482b7cf198699c1432f28d
Vinicius <vinisaraiva@gmail.com>
2026-09-25 04:54:13 -0300

    feat(worker): dispatch social publication jobs

--- worker/src/handlers.ts
+++ worker/src/handlers.ts
@@ -1,10 +1,13 @@
 import type { WorkerJob } from "./queue.js";
 import { handleMediaAnalysis } from "./media-analysis.js";
+import { handleSocialPublish } from "./social-publish.js";
 
 export async function handleJob(job: WorkerJob) {
   switch (job.type) {
     case "media_analysis":
       return handleMediaAnalysis(job);
+    case "social_publish":
+      return handleSocialPublish(job);
     default:
       throw new Error(`Unsupported job type: ${job.type}`);
   }
